c5a969faef862cdb060ec4a13d5d4a5d2df3324d
SugarP1g <zhuchenhui1990@163.com>
2020-08-11 11:55:39 +0800

    Create panic_and_recover.go

---
+++ src/panic/panic_and_recover.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func bad_call() {
+	panic("bad end.")
+}
+
+func test() {
+	defer func() {
+		if e := recover(); e != nil {
+			fmt.Printf("Panicing %s\r\n", e)
+		}
+	}()
+	bad_call()
+	fmt.Printf("After bad call\r\n")
+}
+
+func main() {
+	fmt.Printf("Calling test\r\n")
+	test()
+	fmt.Printf("Test completed\r\n")
+}
